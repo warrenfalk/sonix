a27b0e3f928deff2189f1133c3a20249d17dd518
Warren Falk <warren@warrenfalk.com>
2023-01-29 11:19:44 -0500

    fix behavior of touches for selecting and seeking

--- src/Exchange.tsx
+++ src/Exchange.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Word } from './Word';
 import { TimeRange, formatTimestamp, rangesEqual } from './TranscriptScreen';
 
@@ -11,13 +11,14 @@ type ExchangeProps = {
   onSeekTo: (time: number) => void;
 };
 export const Exchange = React.memo(function ({ ts, sn, ws, currentWord, onCurrent, onSeekTo }: ExchangeProps) {
+  const [selectable, setSelectable] = useState(false);
   return (
     <div className="exchange">
       <div className="meta">
         <div className="speakerName">{sn}</div>
         <div className="time">{formatTimestamp(ts[0])}</div>
       </div>
-      <div className="content">
+      <div className="content" style={{userSelect: selectable === false ? "none" : "text"}}>
         <p className="text">
           {ws.map(([range, word], i) => (
             <Word
@@ -26,7 +27,11 @@ export const Exchange = React.memo(function ({ ts, sn, ws, currentWord, onCurren
               key={i}
               current={rangesEqual(range, currentWord)}
               word={word}
-              onCurrent={onCurrent} />
+              onCurrent={onCurrent}
+              enableSelection={(value) => {
+                setSelectable(value);
+              }}
+            />
           ))}
         </p>
       </div>
--- src/Word.tsx
+++ src/Word.tsx
@@ -8,23 +8,55 @@ type WordProps = {
   start: number;
   onCurrent: (rect: DOMRect, word: string) => void;
   onSeekTo: (time: number) => void;
+  enableSelection: (value: boolean) => void;
 };
-export const Word = React.memo(function ({ current, onSeekTo, start, word, onCurrent }: WordProps) {
+export const Word = React.memo(function ({ current, onSeekTo, start, word, onCurrent, enableSelection }: WordProps) {
   const spb = /^\s*/.exec(word)?.[0] || '';
   const spe = /\s*$/.exec(word)?.[0] || '';
   const trimmed = word.trim();
   const span = useRef<HTMLSpanElement>(null);
+  const disableSeek = useRef(false);
   useEffect(() => {
     if (current) {
       span.current && onCurrent(span.current.getBoundingClientRect(), word);
     }
   }, [current]);
   return (
-    <span onClick={() => onSeekTo(start)}>
+    <span
+      onClick={() => {
+        if (!disableSeek.current) {
+          onSeekTo(start)
+        }
+      }}>
       {spb.length ? <span>{spb}</span> : null}
       <span
         ref={span}
-        className={classNames("word", { current })}>
+        className={classNames("word", { current })}
+        onTouchStartCapture={(e) => {
+          // if something was already selected, this will be a de-select
+          // we don't want this to result in a seek, so the user can safely touch to deselect
+          // without seeking
+          if (getSelection()?.rangeCount) {
+            disableSeek.current = true;
+          }
+          // always deselect anything that was already selected
+          getSelection()?.removeAllRanges();
+          // and enable selection so that if this is the start of a long press
+          // it will result in selection
+          enableSelection(true);
+        }}
+        onTouchEnd={() => {
+          // if we disabled the seek, re-enable it, but not immediately because click still hasn't fired
+          if (disableSeek.current) {
+            setTimeout(() => {
+              disableSeek.current = false;
+            }, 50);
+          }
+          // turn selection back off so that one-click selection doesn't occur
+          // (don't know about other scenarios, but if chrome's translation interface is enbled,
+          //  single clicks will select text which interferes with the behavior)
+          enableSelection(false);
+        }}>
         {trimmed}
       </span>
       {spe.length ? <span>{spe}</span> : null}
